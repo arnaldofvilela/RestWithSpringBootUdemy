81c39cb2ed7119e5a60fba1cb208e09ace9acca4
Arnaldo Neto <vilela-neto@hotmail.com>
2021-05-02 14:42:21 -0300

    Aula 66

---
+++ src/main/java/br/com/arnaldo/services/PersonServices.java
@@ -0,0 +1,45 @@
+package br.com.arnaldo.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.com.arnaldo.model.Person;
+
+@Service
+public class PersonServices {
+
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Person findById(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Arnaldo");
+		person.setLastName("Neto");
+		person.setAddress("Uberlândia - Minas Gerais - Brazil");
+		person.setGender("Male");
+		return person;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name" + i);
+		person.setLastName("Last name" + i);
+		person.setAddress("Some address in Brazil" + i);
+		person.setGender("Male");
+		return person;
+	}
+	
+}
